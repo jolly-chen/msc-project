0027973580db43510f7cc6611d7942344a847847
Jolly Chen <jolly.chen@student.uva.nl>
2023-11-07 21:48:07 +0100

    Also save total time

--- cpu-sequential/benchmark.py
+++ cpu-sequential/benchmark.py
@@ -15,7 +15,7 @@ from tqdm.auto import tqdm
 def run_benchmark(f, n, environs, bulksizes, nbins, nvals, output_file=""):
     if not Path(output_file).exists():
         with open(output_file, "w") as file_handler:
-            file_handler.write("iter,env,nbins,bulksize,input,tfindbin,tfill,tstats\n")
+            file_handler.write("iter,env,nbins,bulksize,input,tfindbin,tfill,tstats,ttotal\n")
 
     with open(output_file, "a") as file_handler:
         for iter in tqdm(range(n)):
@@ -24,7 +24,7 @@ def run_benchmark(f, n, environs, bulksizes, nbins, nvals, output_file=""):
                     for nbi, nb in enumerate(nbins):
                         for nvi, nv in enumerate(nvals):
                             input_file = f"../input/doubles_uniform_{nv}.root"
-                            arg = f"-b{b} -h{nb} -f{input_file}"
+                            arg = f"-b{b} -h{nb} -f{input_file} -e"
                             print(arg)
 
                             r = subprocess.run([f, *arg.split()],
@@ -33,16 +33,13 @@ def run_benchmark(f, n, environs, bulksizes, nbins, nvals, output_file=""):
                                 stdout=subprocess.PIPE,
                                 stderr=subprocess.DEVNULL
                                 )
-                            output = r.stdout.decode("utf-8").split()
-                            times =  [output[i].split(":")[1] for i in range(3)]
+                            output = r.stdout.decode("utf-8").strip().split("\n")
+                            times =  [o.split(":")[1] for o in output]
                             file_handler.write(f"{iter},{e},{nb},{b},{input_file},{','.join(times)}\n")
                             file_handler.flush()
 
-    # Create a dictionary with file names as keys and a dataframe containing the statistics for each kernel/api call.
-    return
+
 if __name__ == "__main__":
-    # environ = sys.argv[1]
-    # os.environ[envion] = "1"
     environs = [
    	"CPU",
 #        "CUDA_HIST",
@@ -53,13 +50,13 @@ if __name__ == "__main__":
     nbins = [
         1,
         2,
-        5,
-        10,
-        20,
-        50,
-        100,
-        500,
-        1000,
+        # 5,
+        # 10,
+        # 20,
+        # 50,
+        # 100,
+        # 500,
+        # 1000,
         # 5000,
         # 10000,
         # 50000,
@@ -67,7 +64,7 @@ if __name__ == "__main__":
 
     bulksizes = [
         1,
-        # 2,
+        2,
         # 4,
         # 8,
         # 16,
@@ -88,14 +85,26 @@ if __name__ == "__main__":
     ]
 
     nvals = [
-        # 1,
-        100000000,    # 100M
-        500000000,    # 500M
-        1000000000,   # 1B
-        5000000000,   # 5B
+        1,
+        # 10,
+        # 100,
+        # 1000,         # 1K
+        # 10000,        # 10K
+        # 100000,       # 100K
+        # 1000000,      # 1M
+        # 10000000,     # 10M
+        # 100000000,    # 100M
+        # 500000000,    # 500M
+        # 1000000000,   # 1B
+        # 5000000000,   # 5B
         # 10000000000,  # 10B
     ]
     f = "./histond_benchmark"
+    if len(sys.argv) > 1:
+        output_file = sys.argv[1]
+    else:
+        output_file = f"walltime-{time.strftime('%Y%m%d-%H%M%S')}"
+    print(f"writing results to {output_file}...")
 
     run_benchmark(
         f,
@@ -104,5 +113,5 @@ if __name__ == "__main__":
         bulksizes,
         nbins,
         nvals,
-        f"{'%s/'.format(sys.argv[1]) if len(sys.argv) > 1 else ''}walltime-{time.strftime('%Y%m%d-%H%M%S')}",
+        output_file
     )
