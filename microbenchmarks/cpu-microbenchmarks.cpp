9eeebd6ed2bb036cf4f259808aaa753da0fe4745
Jolly Chen <jolly.chen@student.uva.nl>
2024-01-16 11:51:02 +0100

    Delete old notebooks

--- microbenchmarks/cpu-microbenchmarks.cpp
+++ microbenchmarks/cpu-microbenchmarks.cpp
@@ -3,16 +3,81 @@
 #include <algorithm>
 #include <random>
 #include <thread>
+#include <iostream>
+#include <stdexcept>
+#include <limits>
+#include <new>
 
 using Clock = std::chrono::steady_clock;
 using fsecs = std::chrono::duration<double>;
 constexpr int repetitions = 1e4;
 
+/**
+ * Returns aligned pointers when allocations are requested. Default alignment
+ * is 64B = 512b, sufficient for AVX-512 and most cache line sizes.
+ *
+ * @tparam ALIGNMENT_IN_BYTES Must be a positive power of 2.
+ */
+template <typename ElementType, std::size_t ALIGNMENT_IN_BYTES = 64>
+class AlignedAllocator {
+private:
+   static_assert(ALIGNMENT_IN_BYTES >= alignof(ElementType),
+                 "Beware that types like int have minimum alignment requirements "
+                 "or access will result in crashes.");
+
+public:
+   using value_type = ElementType;
+   static std::align_val_t constexpr ALIGNMENT{ALIGNMENT_IN_BYTES};
+
+   /**
+    * This is only necessary because AlignedAllocator has a second template
+    * argument for the alignment that will make the default
+    * std::allocator_traits implementation fail during compilation.
+    * @see https://stackoverflow.com/a/48062758/2191065
+    */
+   template <class OtherElementType>
+   struct rebind {
+      using other = AlignedAllocator<OtherElementType, ALIGNMENT_IN_BYTES>;
+   };
+
+public:
+   constexpr AlignedAllocator() noexcept = default;
+
+   constexpr AlignedAllocator(const AlignedAllocator &) noexcept = default;
+
+   template <typename U>
+   constexpr AlignedAllocator(AlignedAllocator<U, ALIGNMENT_IN_BYTES> const &) noexcept
+   {
+   }
+
+   [[nodiscard]] ElementType *allocate(std::size_t nElementsToAllocate)
+   {
+      if (nElementsToAllocate > std::numeric_limits<std::size_t>::max() / sizeof(ElementType)) {
+         throw std::bad_array_new_length();
+      }
+
+      auto const nBytesToAllocate = nElementsToAllocate * sizeof(ElementType);
+      return reinterpret_cast<ElementType *>(::operator new[](nBytesToAllocate, ALIGNMENT));
+   }
+
+   void deallocate(ElementType *allocatedPointer, [[maybe_unused]] std::size_t nBytesAllocated)
+   {
+      /* According to the C++20 draft n4868 § 17.6.3.3, the delete operator
+       * must be called with the same alignment argument as the new expression.
+       * The size argument can be omitted but if present must also be equal to
+       * the one used in new. */
+      ::operator delete[](allocatedPointer, ALIGNMENT);
+   }
+};
+template<typename T, std::size_t ALIGNMENT_IN_BYTES = 64>
+using AlignedVector = std::vector<T, AlignedAllocator<T, ALIGNMENT_IN_BYTES> >;
+
+
 static void BM_Overhead(benchmark::State &state)
 {
    for (auto _ : state) {
       auto start = Clock::now();
-         std::this_thread::sleep_for(std::chrono::milliseconds(1));
+      std::this_thread::sleep_for(std::chrono::milliseconds(1));
       auto end = Clock::now();
       auto elapsed_seconds = std::chrono::duration_cast<fsecs>(end - start);
       state.SetIterationTime(elapsed_seconds.count());
@@ -103,9 +168,8 @@ static void BM_BinaryValidation(benchmark::State &state)
    // Setup assumes binedges is in the cache and the val is in thel register.
    std::vector<double> binedges;
    for (auto i = 0; i < nbins; i++)
-      binedges.emplace_back(i * 1./nbins);
+      binedges.emplace_back(i * 1. / nbins);
    auto a_binedges = binedges.data();
-
 
    for (auto _ : state) {
       double val = rand();
@@ -132,15 +196,19 @@ static void BM_BinarySearchH12(benchmark::State &state)
    long long bin;
    long long nvals = 50e6;
    int nbins = state.range(0);
-   double val = state.range(1)/4.;
+   double val = state.range(1) / 4.;
 
    // Setup assumes binedges is in the cache
    // TODO: try aligning
    // TODO: try pinning
    // TODO: try linear search? reuse distance
-   std::vector<double> binedges;
+   AlignedVector<double, 64> binedges(nbins);
+   if (reinterpret_cast<std::uintptr_t>( binedges.data() ) % 64 != 0 ) {
+        std::cerr << "Vector buffer is not aligned!\n";
+        throw std::logic_error( "Faulty implementation!" );
+    }
    for (auto i = 0; i < nbins; i++)
-      binedges.emplace_back(i * 1./nbins);
+      binedges.emplace_back(i * 1. / nbins);
    auto a_binedges = binedges.data();
 
    for (auto _ : state) {
@@ -166,9 +234,8 @@ static void BM_BinarySearchH3(benchmark::State &state)
    // Setup assumes binedges is in the cache
    std::vector<double> binedges;
    for (auto i = 0; i < nbins; i++)
-      binedges.emplace_back(i * 1./nbins);
+      binedges.emplace_back(i * 1. / nbins);
    auto a_binedges = binedges.data();
-
 
    for (auto _ : state) {
       for (long i = 0; i < nvals; i++) {
@@ -187,10 +254,10 @@ static void BM_BinarySearchH4(benchmark::State &state)
 {
    long long bin;
    int nbins = state.range(0);
-   double range = state.range(1)/4.;
+   double range = state.range(1) / 4.;
    int maxbin = nbins * range;
-   long long nvals = 50e6/maxbin;
-   double stride = 1./nbins;
+   long long nvals = 50e6 / maxbin;
+   double stride = 1. / nbins;
 
    // Setup assumes binedges is in the cache
    std::vector<double> binedges;
@@ -201,7 +268,7 @@ static void BM_BinarySearchH4(benchmark::State &state)
    for (auto _ : state) {
       for (long i = 0; i < nvals; i++) {
          for (long b = 0; b < maxbin; b++) {
-            bin = BinarySearch(nbins, a_binedges, b*stride);
+            bin = BinarySearch(nbins, a_binedges, b * stride);
          }
       }
       static_cast<void>(bin); // prevent bin from being optimized away
@@ -212,7 +279,6 @@ BENCHMARK(BM_BinarySearchH4)
    ->ArgsProduct({{8, 1024, 131072, 16777216},
                   {1, 2, 3, 4}}) // Args only accepts integer, so this is a hacky way to get [0, 0.5, 1]
    ->Unit(benchmark::kSecond);
-
 
 int nbins = 1024;
 int multiplier = 32;
