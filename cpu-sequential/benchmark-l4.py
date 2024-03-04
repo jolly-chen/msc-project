160888104dfd60b4df022aae2d2d6c3889e857c6
Jolly Chen <jolly@olice-05.cern.ch>
2024-03-04 10:29:34 +0100

    [cpu][l4] add multithreaded validation data

--- cpu-sequential/benchmark-l4.py
+++ cpu-sequential/benchmark-l4.py
@@ -6,9 +6,9 @@ import os
 import sys
 import time
 
-base_header = "iter,env,nbins,bulksize,input,edges,tfindbin,tfill,tstats,ttotal"
+base_header = "iter,env,nthreads,nbins,bulksize,input,edges,tfindbin,tfill,tstats,ttotal"
 
-def run_benchmark(f, n, environs, bulksizes, nbins, input_files, output_file=""):
+def run_benchmark(f, n, environs, nthreads, bulksizes, nbins, input_files, output_file=""):
     if not Path(output_file).exists():
         with open(output_file, "w") as file_handler:
             file_handler.write(
@@ -18,67 +18,68 @@ def run_benchmark(f, n, environs, bulksizes, nbins, input_files, output_file="")
     with open(output_file, "a") as file_handler:
         for iter in range(n):
             for ei, e in enumerate(environs):
-                for nbi, nb in enumerate(nbins):
-                    for bi, b in enumerate(bulksizes):
-                        for edges in ["", "-e"]:
-                            procs = []
-                            write_result = '-w' if iter == 0 and bi == 0 else ''
-                            for nif, ipf in enumerate(input_files):
-                                input_file = f"{input_folder}/{ipf}"
-                                stem = Path(ipf).stem
-                                cmd = f""
-                                arg = f"-b{b} -h{nb} -f{input_file} {edges} {write_result}"
-                                print(f"{cmd} {f} {arg}")
+               for ti, nt in enumerate(nthreads):
+                   for nbi, nb in enumerate(nbins):
+                       for bi, b in enumerate(bulksizes):
+                           for edges in ["", "-e"]:
+   #                            procs = []
+                               write_result = '-w' if iter == 0 and bi == 0 and ti == 0 else ''
+                               for nif, ipf in enumerate(input_files):
+                                   input_file = f"{input_folder}/{ipf}"
+                                   stem = Path(ipf).stem
+                                   cmd = f""
+                                   arg = f"-b{b} -h{nb} -f{input_file} {edges} {write_result} -t{nt}"
+                                   print(f"{cmd} {f} {arg}")
 
-                                p = subprocess.Popen(
-                                    [*f"{cmd} {f} {arg}".split()],
-                                    env={**os.environ, e: "1"},
-                                    # check=True,
-                                    stdout=subprocess.PIPE,
-                                    # shell=True,
-                                )
-                                procs.append((p, stem))
+                                   p = subprocess.Popen(
+                                       [*f"{cmd} {f} {arg}".split()],
+                                       env={**os.environ, e: "1"},
+                                       # check=True,
+                                       stdout=subprocess.PIPE,
+                                       # shell=True,
+                                   )
+#                                   procs.append((p, stem))
 
-                            for p, stem in procs:
-                                # r.wait()
-                                # output = r.stdout.decode("utf-8").strip().split("\n")
-                                output, stderr = p.communicate()
+   #                            for p, stem in procs:
+                                   # r.wait()
+                                   # output = r.stdout.decode("utf-8").strip().split("\n")
+                                   output, stderr = p.communicate()
 
-                                if stderr:
-                                    print("JOB FAILED:")
-                                    print(stderr)
-                                else:
-                                    output = output.decode("utf-8").strip().split("\n")
-#                                    print(output)
-                                    times = [o.split(":")[1] for o in output]
-                                    file_handler.write(
-                                        f"{iter},{e},{nb},{b},{stem},{'True' if edges != '' else 'False'},{','.join(times)}\n"
-                                    )
-                                    file_handler.flush()
+                                   if stderr:
+                                       print("JOB FAILED:")
+                                       print(stderr)
+                                   else:
+                                       output = output.decode("utf-8").strip().split("\n")
+   #                                    print(output)
+                                       times = [o.split(":")[1] for o in output]
+                                       file_handler.write(
+                                           f"{iter},{e},{nt},{nb},{b},{stem},{'True' if edges != '' else 'False'},{','.join(times)}\n"
+                                       )
+                                       file_handler.flush()
 
-                                    if write_result:
-                                        r_file = f"{stem}_h{nb}_e{'1' if edges != '' else '0'}"
-                                        print(f"zstd --rm -f -o {input_folder}/expected/{r_file} {r_file}.out"),
-                                        subprocess.run(
-                                                f"zstd --rm -f -o {input_folder}/expected/{r_file} {r_file}.out",
-                                            # [
-                                            #     "zstd",
-                                            #     "--rm",
-                                            #     f"{r_file}.out",
-                                            #     f"-o{r_file}",
-                                            # ],
-                                            check=True,
-                                            shell=True,
-                                        )
+                                       if write_result:
+                                           r_file = f"{stem}_h{nb}_e{'1' if edges != '' else '0'}"
+                                           print(f"zstd --rm -f -o {input_folder}/expected/{r_file} {r_file}.out"),
+                                           subprocess.run(
+                                                   f"zstd --rm -f -o {input_folder}/expected/{r_file} {r_file}.out",
+                                               # [
+                                               #     "zstd",
+                                               #     "--rm",
+                                               #     f"{r_file}.out",
+                                               #     f"-o{r_file}",
+                                               # ],
+                                               check=True,
+                                               shell=True,
+                                           )
 
-                                    # subprocess.run(
-                                    #     [
-                                    #         "mv",
-                                    #         f"{stem}_h{nb}_e{'1' if edges != '' else '0'}.out",
-                                    #         f"{input_folder}/expected/{stem}_h{nb}_e{'1' if edges != '' else '0'}",
-                                    #     ],
-                                    #     check=True,
-                                    # )
+                                       # subprocess.run(
+                                       #     [
+                                       #         "mv",
+                                       #         f"{stem}_h{nb}_e{'1' if edges != '' else '0'}.out",
+                                       #         f"{input_folder}/expected/{stem}_h{nb}_e{'1' if edges != '' else '0'}",
+                                       #     ],
+                                       #     check=True,
+                                       # )
 
 
 if __name__ == "__main__":
@@ -105,25 +106,28 @@ if __name__ == "__main__":
     ]
 
     bulksizes = [
-               1,
+#               1,
         # 2,
         # 4,
-               8,
+#               8,
         # 16,
         # 32,
-               64,
+#               64,
         # 128,
         # 256,
-               512,
+#               512,
         # 1024,
         # 2048,
-               4096,
-        # 8192,
-        # 16384,
-               32768,
-        # 65536,
-        # 131072,
+#               4096,
+               8192,
+               16384,
+#               32768,
+               65536,
+               131072,
                262144,
+               2097152,
+               16777216,
+               134217728,
     ]
 
     input_files = [
@@ -148,17 +152,25 @@ if __name__ == "__main__":
         "doubles_normal-0.7-0.01_1000000000.root",  # 1B
     ]
 
+    num_threads = [
+        1,
+#        8,
+#        16,
+#        32,
+#        64
+    ]
+
     input_folder = "/data/jolly/input"
-    f = "/data/jolly/msc-project-sm86/benchmarks/histond_benchmark"
+    f = "/data/jolly/msc-project-sm89/benchmarks/histond_benchmark"
     if len(sys.argv) > 1:
         output_file = sys.argv[1]
     else:
         output_file = f"{time.strftime('%Y%m%d-%H%M%S')}"
 
-    output_folder = "/data/jolly/msc-project-sm86/cpu-sequential/l4-cpu"
+    output_folder = "/data/jolly/msc-project-sm89/cpu-sequential/l4-cpu"
     os.makedirs(output_folder, exist_ok=True)
     print(f"writing results to {output_folder}/{output_file}...")
 
     run_benchmark(
-        f, n, environs, bulksizes, nbins, input_files, f"{output_folder}/{output_file}"
+        f, n, environs, num_threads, bulksizes, nbins, input_files, f"{output_folder}/{output_file}"
     )
